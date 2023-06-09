78dbb859273d4d8cfa1b8c4636193598314b2e3b
Leigh MacDonald <leigh.macdonald@gmail.com>
2023-06-09 05:56:38 -0600

    Add reverse migration

---
+++ migrations/0001_create_player_table.down.sql
@@ -0,0 +1,12 @@
+begin;
+
+drop table if exists sb_ban;
+drop table if exists sb_site;
+drop table if exists team;
+drop table if exists league;
+drop table if exists player_vanity;
+drop table if exists player_avatars;
+drop table if exists player_names;
+drop table if exists player;
+
+commit;
